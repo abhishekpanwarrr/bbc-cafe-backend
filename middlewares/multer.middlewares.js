05566b210bea4c69b21e05374af426e9e8d55b67
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2023-11-26 11:52:39 +0530

    multer

--- middlewares/multer.middlewares.js
+++ middlewares/multer.middlewares.js
@@ -1,27 +1,53 @@
 import multer from "multer";
 import fs from "fs";
 
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//         const tempDir = './public/temp';
+
+//     // Check if the directory exists, create it if it doesn't
+//     if (!fs.existsSync(tempDir)) {
+//       try {
+//         fs.mkdirSync(tempDir, { recursive: true });
+//       } catch (error) {
+//         console.error('Error creating directory:', error);
+//         return cb(error, null);
+//       }
+//     }
+
+//     cb(null, tempDir);
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// export const upload = multer({
+//   storage,
+// });
+
+const targetDir = "./public/temp";
+
+// Ensure that the parent directory exists
+const parentDir = targetDir.substring(0, targetDir.lastIndexOf("/"));
+if (!fs.existsSync(parentDir)) {
+  fs.mkdirSync(parentDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const tempDir = './public/temp';
-
-    // Check if the directory exists, create it if it doesn't
-    if (!fs.existsSync(tempDir)) {
-      try {
-        fs.mkdirSync(tempDir, { recursive: true });
-      } catch (error) {
-        console.error('Error creating directory:', error);
-        return cb(error, null);
-      }
+    // Check if the target directory exists, create it if it doesn't
+    if (!fs.existsSync(targetDir)) {
+      fs.mkdirSync(targetDir);
     }
 
-    cb(null, tempDir);
+    cb(null, targetDir);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
 });
 
-export const upload = multer({
-  storage,
-});
+const upload = multer({ storage });
+
+module.exports = upload;
